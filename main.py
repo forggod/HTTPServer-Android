4894542e6f51b16c1ea9fd05bdfe298c662f9002
Фёдор Ситников <mr.prizrek@mail.ru>
2023-06-10 19:19:18 +0300

    Added POST and GET methods

--- main.py
+++ main.py
@@ -3,48 +3,84 @@ import socketserver
 import json
 
 PORT = 5050
-IP = '192.168.0.105'
+IP = '192.168.0.100'
 
 
-def open_json():
-    with open('data.json') as file:
+def read_json():
+    with open('data.json', 'r') as file:
         data = json.load(file)
     return data
 
 
-class MyHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        # Определяем заголовки ответа
-        if self.path == '/faculties':
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
+def write_json(data):
+    with open('data.json', 'w') as file:
+        json.dump(data, file)
 
-            # Создаем и отправляем JSON-ответ
-            faculties = open_json()['faculties']
-            data = {"faculties": faculties}
-            json_data = json.dumps(data)
-            self.wfile.write(json_data.encode())
+
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            # Определяем заголовки ответа
+            if self.path == '/':
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+
+                # Создаем и отправляем JSON-ответ
+                data = read_json()
+                print(data)
+                json_data = json.dumps(data)
+                self.wfile.write(json_data.encode())
+
+            elif self.path == '/university/faculties' or \
+                    self.path == '/university/groups' or \
+                    self.path == '/university/students':
+                paths = self.path.split('/')
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+
+                data = read_json()[paths[1]][paths[2]]
+                json_data = json.dumps(data)
+                print(json_data)
+                self.wfile.write(json_data.encode())
+
+            else:
+                self.send_error(404, 'No resource on that address')
+
+        except Exception as e:
+            self.send_error(404, str(e))
 
     def do_POST(self):
-        # Получаем данные тела запроса
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
+        try:
+            if self.path == '/university/faculties' or \
+                    self.path == '/university/groups' or \
+                    self.path == '/university/students':
+                paths = self.path.split('/')
+                # Получаем данные тела запроса
+                content_length = int(self.headers['Content-Length'])
+                post_data = self.rfile.read(content_length)
 
-        # Обрабатываем данные и сохраняем в файл
-        data = json.loads(post_data.decode())
-        with open('data.json', 'w') as file:
-            json.dump(data, file)
+                # Обрабатываем данные и сохраняем в файл
+                data = read_json()
+                data[paths[1]][paths[2]] = json.loads(post_data.decode())
+                write_json(data)
+                print(json.dumps(data))
 
-        # Определяем заголовки ответа
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
+                # Определяем заголовки ответа
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
 
-        # Создаем и отправляем JSON-ответ
-        message = {"message": "Data saved succesfully"}
-        json_data = json.dumps(message)
-        self.wfile.write(json_data.encode())
+                # Создаем и отправляем JSON-ответ
+                message = {"message": "Data saved succesfully"}
+                json_data = json.dumps(message)
+                self.wfile.write(json_data.encode())
+            else:
+                self.send_error(404, 'No resource on that address')
+
+        except Exception as e:
+            self.send_error(404, str(e))
 
 
 handler = MyHandler
